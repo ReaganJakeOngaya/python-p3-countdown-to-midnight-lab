abae2fb058b4208bcb16eeb7258a4ec781ebaebe
Reagan-Ongaya <reagan.ongaya@student.moringaschool.com>
2024-10-17 19:42:21 +0300

    while lopps quiz

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,10 @@
 # your code goes here!
+def countdown():
+    x = 10
+    while x >= 0:
+        if x == 0:
+            print("HAPPY NEW YEAR")
+        else:
+            print(f'{x} SECOND(S)!')
+        x -= 1
+countdown()
